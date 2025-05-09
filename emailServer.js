8c57cd58c0ad22bd8bd8e9d64c5e1229e466bd8e
bhavini0805 <reachbhavini@gmail.com>
2025-05-09 14:50:01 -0400

    getting data from api and email info

--- emailServer.js
+++ emailServer.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const express = require('express');
+const fetch = require('node-fetch');
+const nodemailer = require('nodemailer');
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './templates');
@@ -19,4 +21,38 @@ app.get("/", (request, response) => {
 
 app.get("/signup", (request, response) => {
 
+});
+
+app.post("/send-email", async (req, res) => {
+    const userEmail = "form input that doesn't exist yet"; // FIX THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!J
+    try {
+        const quoteRes = await fetch('https://api.kanye.rest');
+        const data = await quoteRes.json();
+        const quote = data.quote;
+
+        // sentiment analysis
+
+        //send emails
+        const transport = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'kanyespams335@gmail.com',
+                pass: 'imisstheoldkanye335'
+            }
+        });
+
+        for (let i = 0; i < 5; i++) { // send 5 spam emails, change later with form input
+            await transport.sendMail({
+                from: 'kanyespams335@gmail.com',
+                to: userEmail,
+                subject: 'kanye says...',
+                text: quote
+            });
+        }
+
+        res.send("rip email recipient");
+    } catch (error) {
+        console.error("there was an error :/");
+        console.error(error);
+    }
 });
