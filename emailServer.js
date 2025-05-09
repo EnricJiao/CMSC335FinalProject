beadc6d1c981fe9deb5e55828da0eb4a18983491
enric <enricjiao3@gmail.com>
2025-05-09 14:06:55 -0400

    setup + signup page

---
+++ emailServer.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', './templates');
+
+
+//temporary port for local development
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`Web server started and running at http://localhost:${port}`);
+});
+
+app.get("/", (request, response) => {
+    response.render("signup");  
+});
